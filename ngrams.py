091b1f93dabdd3635275d3a5b1c6295a1747fe5b
stlgolfer <stlgolfer@users.noreply.github.com>
2023-04-08 22:28:13 -0400

    Started ML implementation

---
+++ ngrams.py
@@ -0,0 +1,93 @@
+import keras.models
+import pandas as pd
+import os
+import numpy as np
+
+import tensorflow as tf
+from keras_preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, LSTM, Dense, Bidirectional
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import nltk; nltk.download('punkt')
+import convokit
+from convokit import Corpus, download
+import pickle
+
+# code mostly from https://www.kaggle.com/code/ysthehurricane/next-word-prediction-bi-lstm-tutorial-easy-way/input
+# good data set possibly from cornell: https://github.com/niderhoff/nlp-datasets
+# http://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html
+# https://github.com/CornellNLP/ConvoKit/tree/0e44dd838df191a22c6ba0548a44f8e3d26d34cb
+# medium_data = pd.read_csv('./medium_data.csv')
+# medium_data.head()
+
+model = None
+print(len(os.listdir('./model_trained')))
+# make and train the model
+# will need to do a bit of preprocessing to connect cornell db to regular db
+corpus = Corpus(download('movie-corpus'))
+df = corpus.get_utterances_dataframe().iloc[0:100, :] # gotta make this smaller somehow ig
+key = 'text'
+
+
+print("Number of records: ", df.shape[0])
+print("Number of fields: ", df.shape[1])
+
+df[key]
+df[key] = df[key].apply(lambda x: x.replace(u'\xa0',u' '))
+df[key] = df[key].apply(lambda x: x.replace('\u200a',' '))
+tokenizer = Tokenizer(oov_token='<oov>') # For those words which are not found in word_index
+tokenizer.fit_on_texts(df[key])
+total_words = len(tokenizer.word_index) + 1
+
+print("Total number of words: ", total_words)
+print("Word: ID")
+print("------------")
+# print("<oov>: ", tokenizer.word_index['<oov>'])
+# print("Strong: ", tokenizer.word_index['strong'])
+# print("And: ", tokenizer.word_index['and'])
+
+input_sequences = []
+for line in df[key]:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    # print(token_list)
+
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i + 1]
+        input_sequences.append(n_gram_sequence)
+
+# print(input_sequences)
+print("Total input sequences: ", len(input_sequences))
+
+# pad sequences
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+input_sequences[1]
+
+# need to cache input_sequences and the tokenizer to speed up load times
+with open('./tokenizer.pkl', 'wb') as f:
+    pickle.dump({'tokenizer': tokenizer, 'max_sequence_len': max_sequence_len}, f)
+
+xs, labels = input_sequences[:,:-1],input_sequences[:,-1]
+ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+
+print(xs[5])
+print(labels[5])
+print(ys[5][14])
+model = Sequential()
+model.add(Embedding(total_words, 100, input_length=max_sequence_len-1))
+model.add(Bidirectional(LSTM(150)))
+model.add(Dense(total_words, activation='softmax'))
+adam = Adam(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+history = model.fit(xs, ys, epochs=1, verbose=1)
+#print model.summary()
+print(model)
+model.save('./model_trained')
+
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.show()
---
+++ predict_from_model.py
@@ -0,0 +1,74 @@
+# we'll need a few things: the dataset and the model, and the necessary imports
+import pickle
+
+import keras.models
+import pandas as pd
+import os
+import numpy as np
+
+import tensorflow as tf
+from keras_preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, LSTM, Dense, Bidirectional
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import nltk; nltk.download('punkt')
+import convokit
+from convokit import Corpus, download, download_local
+
+# corpus = Corpus(download_local('movie-corpus', 'C:/Users/stlgo/.convokit/downloads'))
+# MAX_DIMENSIONS = 100
+# df = corpus.get_utterances_dataframe().iloc[0:MAX_DIMENSIONS, :] # gotta make this smaller somehow ig
+# key = 'text'
+#
+# # need to get tokenizer
+# df[key]
+# df[key] = df[key].apply(lambda x: x.replace(u'\xa0',u' '))
+# df[key] = df[key].apply(lambda x: x.replace('\u200a',' '))
+# tokenizer = Tokenizer(oov_token='<oov>') # For those words which are not found in word_index
+# tokenizer.fit_on_texts(df[key])
+#
+# # region: from generator
+# input_sequences = []
+# for line in df[key]:
+#     token_list = tokenizer.texts_to_sequences([line])[0]
+#     # print(token_list)
+#
+#     for i in range(1, len(token_list)):
+#         n_gram_sequence = token_list[:i + 1]
+#         input_sequences.append(n_gram_sequence)
+#
+# # print(input_sequences)
+# print("Total input sequences: ", len(input_sequences))
+# #endregion
+#
+# # pad sequences
+# max_sequence_len = max([len(x) for x in input_sequences])
+
+# load meta from pickle file
+with open('./tokenizer.pkl', 'rb') as fp:
+    meta = dict(pickle.load(fp))
+    tokenizer = meta['tokenizer']
+    max_sequence_len = meta['max_sequence_len']
+
+def find_next_word(seed_text, next_words):
+    # actually load the model
+    model = keras.models.load_model('./model_trained')
+
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+        # predicted = model.predict_classes(token_list, verbose=0)
+        #TODO: adjust threshold
+        # predicted = (model.predict(token_list) > 0.005).astype("int32")
+        predicted = np.argmax(model.predict(token_list), axis=-1)
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        seed_text += " " + output_word
+    print(seed_text)
+
+print(find_next_word('hello', 1))
